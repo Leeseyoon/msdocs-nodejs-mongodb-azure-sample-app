48194e4f08d796b5b2c8533619ee8f4e0c810a66
Leeseyoon <custmor@kookmin.ac.kr>
2025-02-17 13:16:32 +0900

    cors 업데이트 배포 테스트

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/', function(req, res, next) {
       const currentTasks = tasks.filter(task => !task.completed);
       const completedTasks = tasks.filter(task => task.completed === true);
 
-      console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
+      console.log(`Total tasks: ${tasks.length}  CORS Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
       res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
     })
     .catch((err) => {
